d3d57c1a880db1887348fba624b47da75026b25b
Brian Wigginton <brianwigginton@gmail.com>
2016-07-11 22:12:30 -0500

    refactor kitten script with better docs

--- scripts/kittens.js
+++ scripts/kittens.js
@@ -2,17 +2,19 @@
 //   Bitcoin Price Retriever
 //
 // Commands:
-//   kittens - Returns a random image of a kitten!
+//   kitten - Returns a random image of a kitten!
 
 
 module.exports = function(robot) {
+  var GIPHY_URL = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=';
   robot.hear(/kitten/i, function(msg) {
-    msg.http('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=kitten')
-       .get()(parseGiphyJSON);
 
-    function parseGiphyJSON(err, res, body) {
+    // This is some weird syntax. Note when you want to use get(). You need to
+    // use an extra pair of parens. ex: .get()(function() { ... })
+
+    msg.http(GIPHY_URL + 'kitten').get()(function parseGiphyJSON(err, res, body) {
       var parsedJSON = JSON.parse(body);
       msg.send(parsedJSON.data.image_url)
-    }
+    });
   });
 };
