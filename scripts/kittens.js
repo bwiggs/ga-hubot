dc26314fd96e9a979699d0532b1eeef719e33fe5
Brian Wigginton <brianwigginton@gmail.com>
2016-07-11 21:43:18 -0500

    update kitten docs

--- scripts/kittens.js
+++ scripts/kittens.js
@@ -1,3 +1,10 @@
+// Description:
+//   Bitcoin Price Retriever
+//
+// Commands:
+//   kittens - Returns a random image of a kitten!
+
+
 module.exports = function(robot) {
   robot.hear(/kitten/i, function(msg) {
     msg.http('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=kitten')
