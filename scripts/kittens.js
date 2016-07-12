55b96d3dde00f39e0a7cbc3a8988d9b4d8476054
Brian Wigginton <brianwigginton@gmail.com>
2016-07-11 20:03:07 -0500

    add hubot http code

---
+++ scripts/kittens.js
@@ -0,0 +1,11 @@
+module.exports = function(robot) {
+  robot.hear(/kitten/i, function(msg) {
+    msg.http('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=kitten')
+       .get()(parseGiphyJSON);
+
+    function parseGiphyJSON(err, res, body) {
+      var parsedJSON = JSON.parse(body);
+      msg.send(parsedJSON.data.image_url)
+    }
+  });
+};
